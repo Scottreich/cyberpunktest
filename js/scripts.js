2c0df55dba81b4107c9111a656fd89bc9cb13d85
Scott Reichert <scottreichert86@gmail.com>
2017-09-26 22:28:36 -0700

    remove radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
-    event.preventDefault();
-    $('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
+  event.preventDefault();
+  $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus();
+
+
+
+
+// Use the function below to retrieve value from the specific modals submit button once you straighten out how to get each modal to display different text and information --- must use js/$ //
+  // function myFunction() {
+  //     var mylist = document.getElementById("");
+  //     document.getElementById("demo").value = mylist.options[mylist.selectedIndex].text;
+  // }
+  //
+
+
 })
   });
 });
